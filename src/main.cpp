872dbe2bccf610cfa6e0847e3f215835ea5128d8
Sergi <smaymi@portic.net>
2025-02-11 09:13:13 +0100

    initial black & white

---
+++ src/main.cpp
@@ -0,0 +1,32 @@
+#include <raylib.h>
+#include <iostream>
+#include "mandelbrot.hpp"
+
+int main()
+{
+    constexpr Color darkGray = {22, 22, 22, 255};
+
+    constexpr int screenWidth = 750;
+    constexpr int screenHeight = 750;
+    constexpr int CELL_SIZE = 1;
+
+    InitWindow(screenWidth, screenHeight, "Game of Life");
+    SetTargetFPS(1);
+
+    Mandelbrot mandelbrot = Mandelbrot{screenHeight / CELL_SIZE, screenWidth / CELL_SIZE, {-3.0, 3.0}, {-3.0, 3.0}};
+
+    while (!WindowShouldClose())
+    {
+        // 1 - Event Handling
+
+        // 2 - Update State
+
+        // 3 - Drawing
+        BeginDrawing();
+            ClearBackground(darkGray);
+            mandelbrot.Draw();
+        EndDrawing();
+    }
+
+    CloseWindow();
+}
